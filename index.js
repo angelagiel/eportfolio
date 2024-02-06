0322e4ec90da94521030df8af402e2e31d33b5d6
limlingan <limlinganangela@gmail.com>
2024-02-06 22:51:33 +0800

    -configured a modal__overlay--visible class with .then(), .catch() -setup a fake timeout andm fake data for testing -z-indexes -remove required attribute for easier testing for now

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-// template_u7h4a3h -- template id 
-// service_cj21lld  -- service id 
+// template_u7h4a3h -- template id
+// service_cj21lld  -- service id
 // RRGoQFjdBVXElmRTD -- API key - Public key
 
 function contact(event) {
-    event.preventDefault();
-    emailjs
-        .sendForm(
-            'service_cj21lld',
-            'template_u7h4a3h',
-            event.target, 
-            'RRGoQFjdBVXElmRTD'
-    ).then(() => {console.log('works1')})
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
+  emailjs
+      .sendForm(
+          'service_cj21lld',
+          'template_u7h4a3h',
+          event.target,
+          'RRGoQFjdBVXElmRTD'
+  ).then(() => {
+    // throw new Error('error');
+    loading.classList.remove("modal__overlay--visible")
+    success.classList += " modal__overlay--visible";
+  }).catch(() => {
+    loading.classList.remove("modal__overlay--loading")
+    alert("Email is temporarily unavailable.\nPlease contact me directly on limlinganangela@gmail.com");
+  });
+
 }
+
+// this is a fake time out for fake data to not waste my emailjs email requests
+// event.preventDefault()
+// const loading = document.querySelector(".modal__overlay--loading")
+// const success = document.querySelector('.modal__overlay--success')
+// loading.classList += " modal__overlay--visible"
+
+// setTimeout(() => {
+//     loading.classList.remove("modal__overlay--visible")
+//     success.classList += " modal__overlay--visible"
+// }, 1000)
+
+// "loading is visible once onsubmit after 1 second delay, loading is hidden and then success overlay is shown"
